b52a82964923168964e246f5475998d4338356e4
Miguel Lourenço <miguelsplc@gmail.com>
2020-12-14 22:51:03 +0000

    Criação de modelo Movie
    Foi criada a classe "Movie" e criado uma chamada de teste em javascript  da API do TMDb de modo a que encontre uma lista de filmes com a string fornecida na pagina.

---
+++ MovieShelf/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MovieShelf.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public int RunTime { get; set; }
+        public string Genre { get; set; }
+    }
+}
